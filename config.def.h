d2382455e297f2dcfe80330d911f0051c91cb827
alvsz <102085931+alvsz@users.noreply.github.com>
2022-04-10 15:01:32 -0300

    tag mask

--- config.def.h
+++ config.def.h
@@ -1,37 +1,50 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
+static const unsigned int borderpx  = 2;         /* border pixel of windows */
+static const unsigned int snap      = 32;        /* snap pixel */
+static const unsigned int gappih    = 10;        /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;        /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;        /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;        /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;         /* 1 means no outer gap when there is only one window */
+static const int showbar            = 1;         /* 0 means no bar */
+static const int usealtbar          = 1;         /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
 static const char *altbarcmd        = "/home/mamba/.config/polybar/bar.sh"; /* Alternate bar launch command */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 0;       /* vertical padding of bar */
-static const int sidepad            = 0;       /* horizontal padding of bar */
-static const char *fonts[]          = { "Cantarell:size=13", "Iosevka:size=14", "Font Awesome:size=14" };
+static const int vertpad            = 0;        /* vertical padding of bar */
+static const int sidepad            = 0;        /* horizontal padding of bar */
+static const char *fonts[]          = { "Cantarell:size=13", "Iosevka:size=14" };
 static const char dmenufont[]       = "Cantarell:size=13";
 
 #include "/home/mamba/.cache/wal/colors-wal-dwm.h"
 #include <X11/XF86keysym.h>
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "" };
+static const char *tags[] = { "", "爵", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class                            instance         title                 tags mask     isfloating   monitor */
+	{ "Pwcalculator",                   NULL,            NULL,                 0,            1,           -1 },
+	{ "st-256color",                    NULL,            NULL,                 1,            0,           -1 },
+	{ "firefox",                        NULL,            NULL,                 1 << 1,       0,           -1 },
+	{ "librewolf",                      NULL,            NULL,                 1 << 1,       0,           -1 },
+	{ NULL,                             "Places",        NULL,                 1 << 1,       1,           -1 },
+	{ "Com.github.gabutakut.gabutdm",   NULL,            NULL,                 1 << 1,       0,           -1 },
+	{ "Gedit",                          NULL,            NULL,                 1 << 2,       0,           -1 },
+	{ NULL,                             "libreoffice",   NULL,                 1 << 2,       0,           -1 },
+	{ "Gimp",                           NULL,            NULL,                 1 << 2,       0,           -1 },
+	{ "Org.gnome.Nautilus",             NULL,            NULL,                 1 << 3,       0,           -1 },
+	{ "Geary",                          NULL,            NULL,                 1 << 4,       0,           -1 },
+	{ "Lutris",                         NULL,            NULL,                 1 << 5,       0,           -1 },
+	{ "Steam",                          NULL,            NULL,                 1 << 5,       0,           -1 },
+	{ "Steam",                          NULL,            "Lista de amigos",    1 << 5,       1,           -1 },
+	{ "Rare",                           NULL,            NULL,                 1 << 5,       0,           -1 },
 };
 
 /* layout(s) */
@@ -45,9 +58,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "  ",      tile },    /* first entry is default */
-	{ "  ",      monocle },
-	{ "  ",      spiral },
+	{ "[]=",      tile },    /* first entry is default */
+	{ "[M]",      monocle },
+	{ "",        spiral },
 	{ "[\\]",     dwindle },
 	{ "H[]",      deck },
 	{ "TTT",      bstack },
@@ -58,8 +71,8 @@ static const Layout layouts[] = {
 	{ ":::",      gaplessgrid },
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
-	{ "  ",      NULL },    /* no layout function means floating behavior */
-	{ NULL,       NULL },
+	{ "",        NULL },    /* no layout function means floating behavior */
+//	{ NULL,       NULL },    
 };
 
 /* key definitions */
@@ -75,8 +88,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]          = { "dmenu_run_i", "-i", "-l", "7", "-g", "2", "-h", "30", "-bw", "5", "-fn", dmenufont, "-p", "Executar: ", "-sb", sel_bg, "-sf", sel_fg, NULL };
-static const char *clipmenucmd[]       = { "clipmenu",    "-i", "-l", "7", "-g", "2", "-h", "40", "-bw", "5", "-fn", dmenufont, "-sb", sel_bg, "-sf", sel_fg, NULL };
+static const char *dmenucmd[]          = { "dmenu_run_i", "-i", "-l", "7", "-g", "2", "-h", "30", "-bw", "5", "-fn", dmenufont, "-p", "Executar:⠀", "-sb", sel_bg, "-sf", sel_fg, NULL };
+static const char *clipmenucmd[]       = { "clipmenu",    "-i", "-l", "7", "-g", "2", "-h", "40", "-bw", "5", "-i", "-fn", dmenufont, "-sb", sel_bg, "-sf", sel_fg, NULL };
 static const char *termcmd[]           = { "st", NULL };
 
 static const char *stop[]              = { "playerctl", "stop", NULL };
@@ -96,7 +109,7 @@ static const char *printareatrans[]    = { "gnome-screenshot", "-a", "-c", NULL 
 static const char *printgui[]          = { "gnome-screenshot", "-i", NULL };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
+	/* modifier                     key                         function        argument */
 	{ MODKEY,                       XK_d,                       spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return,                  spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_v,                       spawn,          {.v = clipmenucmd } },
@@ -110,30 +123,29 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Left,                    setmfact,       {.f = -0.05} },
 	{ MODKEY|ShiftMask,             XK_Right,                   setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return,                  zoom,           {0} },
-	
-	
+
 	{ MODKEY,                       XK_equal,      incrgaps,       {.i = -1 } },
 	{ MODKEY,                       XK_minus,      incrgaps,       {.i = +1 } },
-	
+
 	{ MODKEY|Mod1Mask,              XK_i,      incrigaps,      {.i = +1 } },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } },
 	{ MODKEY|Mod1Mask,              XK_o,      incrogaps,      {.i = +1 } },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } },
-	
+
 	{ MODKEY|Mod1Mask,              XK_6,      incrihgaps,     {.i = +1 } },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } },
 	{ MODKEY|Mod1Mask,              XK_7,      incrivgaps,     {.i = +1 } },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } },
-	
+
 	{ MODKEY|Mod1Mask,              XK_8,      incrohgaps,     {.i = +1 } },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
 	{ MODKEY|Mod1Mask,              XK_9,      incrovgaps,     {.i = +1 } },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
-	
+
 	{ MODKEY|ShiftMask,             XK_equal,      togglegaps,     {0} },
 	{ MODKEY|ShiftMask,             XK_minus,      defaultgaps,    {0} },
-	
-	
+
+
 	{ MODKEY,                       XK_Tab,                     view,           {0} },
 	{ MODKEY,                       XK_q,                       killclient,     {0} },
 	{ MODKEY,                       XK_t,                       setlayout,      {.v = &layouts[0]} },
